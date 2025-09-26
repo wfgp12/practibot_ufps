e74574d83bb5199411f51322e7a258ea8af8098e
wfgp12 <w.gutierrez1206@gmail.com>
2025-09-25 21:16:48 -0500

    feat: Agregar authSlice y eliminar userSlice para simplificar la gestión de autenticación

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,36 @@
+import type { IUser } from "@/models/IUser";
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+
+interface AuthState {
+  user: null | IUser,
+  token: string | null,
+  isAuthenticated: boolean,
+};
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action: PayloadAction<{ user: IUser; token: string }>) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
