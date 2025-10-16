905ccd4104d79df0c4921680b23f8369dce2c07f
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2025-10-16 00:07:01 -0500

    Integrar el widget de chat n8n en la aplicación
    Se añadió la dependencia @n8n/chat y se integró el widget de chat n8n en App.tsx con mensajes iniciales y localización personalizados. Se descontinuaron los componentes ChatbotButton y ChatbotSidebar anteriores en favor del nuevo widget de chat.

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { loadUserThunk } from '@/store/thunks/authThunks';
 
 import { CompanyRegister, Dashboard, HomePage, LoginPage } from '@/pages'
 import { VacancyDetail } from '@/pages/dashboard/components/vancancy/VacancyDetail';
-import { CompanyDetail } from '@/pages/dashboard/components/company/companyDetail';
+import { CompanyDetail } from '@/pages/dashboard/components/company/CompanyDetail';
 import { Layout, PrivateRoute, PublicRoute } from '@/components'
 import LoaderBottomRight from '@/components/Loader';
-import { ChatbotButton } from '@/components/ChatbotButton';
-import { ChatbotSidebar } from '@/components/ChatbotSidebar';
+// import { ChatbotButton } from '@/components/ChatbotButton';
+// import { ChatbotSidebar } from '@/components/ChatbotSidebar';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
 
 import './App.css'
 
@@ -21,6 +23,25 @@ function App() {
   useEffect(() => {
     dispatch(loadUserThunk());
   }, [dispatch]);
+
+  useEffect(() => {
+		createChat({
+			webhookUrl: 'https://n8n.juanpctsoftware.online/webhook/d3694e2f-6241-4822-96b0-cdd33004998e/chat',
+      initialMessages: [
+        '¡Hola! 👋',
+        'Mi nombre es PractiBOT. ¿Cómo puedo ayudarte hoy?'
+      ],
+      i18n: {
+        en: {
+          title: '¡Hola! 👋',
+          subtitle: "Inicia un chat. Estamos aquí para ayudarte 24/7.",
+          footer: '',
+          getStarted: 'Nueva Conversación',
+          inputPlaceholder: 'Escribe tu pregunta..',
+        },
+      },
+		});
+	}, []);
 
   return (
     <>
@@ -62,9 +83,8 @@ function App() {
       </Routes>
 
       <LoaderBottomRight />
-      <ChatbotButton />
-      <ChatbotSidebar />
-
+      {/* <ChatbotButton />
+      <ChatbotSidebar /> */}
       <Toaster
         position="bottom-right"
         richColors
